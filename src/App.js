f20a18970118ae0436b3eef592e5e9b72109d60a
Elif Karadeniz <81620156+elifkaradenizz@users.noreply.github.com>
2023-10-24 19:01:30 +0300

    mini görsel uygulaması yapıldı

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+
+  const pictureIDs= [100,54,122,203,112]
+  const [imageId, setImageId] = useState(200)
+  const getLoremPicsumImg = (id, width = 800, height = 450) => `https://picsum.photos/id/${id}/${width}/${height}`
+
+  function handleClick(id) {
+    setImageId(id)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="container">
+      <hgroup>
+        <h2>Mini Görsel Uygulaması</h2>
+        <h3>Görsel:</h3>
+      </hgroup>
+      <img src={getLoremPicsumImg(imageId)} alt="" style={{ width: '100%' }} />
+      <div class="grid">
+        {
+          pictureIDs.map((item, index) => <div key={index}>
+            <img
+              onClick={() => handleClick(item)}
+              src={getLoremPicsumImg((item), 100, 100)}
+              alt="" />
+          </div>)
+        }
+      </div>
+    </main>
+
   );
 }
+
+
+/*ESKİ YÖNTEM..
+ {
+   [...Array(5)].map((item, index) => <div key={index}>
+     <img 
+     onClick={() => handleClick(200 + index)}
+     src={getLoremPicsumImg((200 + index), 100, 100)}
+      alt="" />
+  </div>)
+ } */
 
 export default App;
